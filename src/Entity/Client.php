5e0e3063541bbcbb543fb2d17fb1b9050c0d9eb6
Kevin Mulot <kevin.mulot@gmail.com>
2021-06-14 06:32:41 +0200

    Small fix

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -147,8 +147,6 @@ class Client implements UserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
